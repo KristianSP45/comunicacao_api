4d9c0fb5630c18c4f0fe9f8c55581617409900c6
Kristian Pessoa <sousakristian45@gmail.com>
2026-04-18 15:24:56 -0300

    Adicionadas dependências e configurado o OpenFeign para API externa de email

--- src/main/java/com/luizalebs/comunicacao_api/business/converter/ComunicacaoConverter.java
+++ src/main/java/com/luizalebs/comunicacao_api/business/converter/ComunicacaoConverter.java
@@ -12,25 +12,26 @@ public class ComunicacaoConverter {
 
     public ComunicacaoEntity paraEntity(ComunicacaoInDTO dto) {
         return ComunicacaoEntity.builder()
-                .dataHoraenvio(dto.getDataHoraEnvio())
-                .emailDestinatario(dto.getEmailDestinatario())
-                .nomeDestinatario(dto.getNomeDestinatario())
-                .mensagem(dto.getMensagem())
-                .modoDeEnvio(dto.getModoDeEnvio())
-                .statusEnvio(dto.getStatusEnvio())
-                .telefoneDestinatario(dto.getTelefoneDestinatario())
+                .dataHoraenvio(dto.getDataHoraEnvio() )
+                .emailDestinatario(dto.getEmailDestinatario() )
+                .nomeDestinatario(dto.getNomeDestinatario() )
+                .mensagem(dto.getMensagem() )
+                .modoDeEnvio(dto.getModoDeEnvio() )
+                .statusEnvio(dto.getStatusEnvio() )
+                .telefoneDestinatario(dto.getTelefoneDestinatario() )
                 .build();
     }
 
     public ComunicacaoOutDTO paraDTO(ComunicacaoEntity entity) {
         return ComunicacaoOutDTO.builder()
-                .dataHoraEnvio(entity.getDataHoraenvio())
-                .emailDestinatario(entity.getEmailDestinatario())
-                .nomeDestinatario(entity.getNomeDestinatario())
-                .mensagem(entity.getMensagem())
-                .modoDeEnvio(entity.getModoDeEnvio())
-                .telefoneDestinatario(entity.getTelefoneDestinatario())
-                .statusEnvio(entity.getStatusEnvio())
+                .id(entity.getId() )
+                .dataHoraEnvio(entity.getDataHoraenvio() )
+                .emailDestinatario(entity.getEmailDestinatario() )
+                .nomeDestinatario(entity.getNomeDestinatario() )
+                .mensagem(entity.getMensagem() )
+                .modoDeEnvio(entity.getModoDeEnvio() )
+                .telefoneDestinatario(entity.getTelefoneDestinatario() )
+                .statusEnvio(entity.getStatusEnvio() )
                 .build();
     }
 }
